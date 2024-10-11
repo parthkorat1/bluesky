e217974b046245b4909029319e26ff05516b82c3
parth <koratparth7@gmail.com>
2024-10-11 20:03:30 +0530

    email

--- src/components/email-template.jsx
+++ src/components/email-template.jsx
@@ -11,12 +11,10 @@ export const EmailTemplate = ({
   message
 }) => (
   <div>
-    <h1>Welcome, {firstName}</h1>
-    <h1>Welcome, {email}</h1>
-    <h1>Welcome, {phone}</h1>
-    <h1>Welcome, {message}</h1>
-
-    <h3> is need to change templet of this email . if yes then inform and no then definetly inform.</h3>
+    <h1>name:- {firstName}</h1>
+    <h1>Email:- {email}</h1>
+    <h1>NUmber:- {phone}</h1>
+    <h1>MEssage:- {message}</h1>
 
     <h2>Regards,</h2>
     <h2>BlueSky Team. </h2>
